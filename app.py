f27e0bfd248e4f6502ca96c3cccdf4ad10399ad0
v1enna <19vienna20@gmail.com>
2022-05-13 23:00:02 +0200

    Check if SOURCE_DIRECTORY && RESULT_DIRECTORY exist

--- app.py
+++ app.py
@@ -6,10 +6,13 @@ result_directory = 'result'
 split = 5
 i = 0
 
-for filename in os.listdir(source_directory):
-    f = os.path.join(source_directory, filename)
+if(os.path.exists(source_directory) and os.path.exists(result_directory)):
+    for filename in os.listdir(source_directory):
+        f = os.path.join(source_directory, filename)
 
-    if(os.path.isfile(f)):
-        video = VideoFileClip(f).subclip(split)
-        video.write_videofile(result_directory + "/result" + str(i) + ".mp4", fps = 25)
-        i = i + 1
+        if(os.path.isfile(f)):
+            video = VideoFileClip(f).subclip(split)
+            video.write_videofile(result_directory + "/result" + str(i) + ".mp4", fps = 25)
+            i = i + 1
+else:
+    print('Missing: ' + source_directory + ' || ' + result_directory)
